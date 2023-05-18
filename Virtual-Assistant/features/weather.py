994eff5306317598e76c0390956231cd5363384a
OJHacker <ojoshi387@gmail.com>
2023-05-18 13:54:26 +0530

    Added Weather Plugin

---
+++ Virtual-Assistant/features/weather.py
@@ -0,0 +1,15 @@
+import requests
+import json
+
+def get_weather(location):
+    api_key = 'YOUR_API_KEY'
+    base_url = 'http://api.openweathermap.org/data/2.5/weather'
+    params = {'q': location, 'appid': api_key, 'units': 'metric'}
+    response = requests.get(base_url, params=params)
+    data = response.json()
+    temperature = str(data['main']['temp'])
+    humidity = str(data['main']['humidity'])
+    weather_conditions = str(data['weather'][0]['description'])
+    print(f"the weather condition in '{location}' city is:'{weather_conditions}' tempearture is:'{temperature}°C' and humidity is:'{humidity}%'")
+    
+
