a557b193162851e12b12d0acb1e7c523ca275954
OJHacker <ojoshi387@gmail.com>
2023-05-18 14:34:59 +0530

    Added takecommand function

--- Virtual-Assistant/Virtual_Assistant.py
+++ Virtual-Assistant/Virtual_Assistant.py
@@ -1,1 +1,21 @@
+from features import weather, websearch, wishme
+from features.wishme import speak
+from webbrowser import open
+import speech_recognition as sr
+import webbrowser
 
+
+def takecommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print('Listening....')
+        r.pause_threshold = 1             #for pausing after how many second's
+        r.adjust_for_ambient_noise(source) #For noise cancelation
+        audio = r.listen(source)
+    try:
+        print('Recognising...')
+        query = r.recognize_google(audio, language='en-us')
+    except Exception as e:
+        print('---')
+        return '---'
+    return f"{query}"
