2a246ec28627183e2a4d863c25f4ab72a13566b8
OJHacker <ojoshi387@gmail.com>
2023-05-20 14:20:54 +0530

    Added test Plugins for opening_web_sites

--- Virtual-Assistant/test/test_features/test_opening_web_sites.py
+++ Virtual-Assistant/test/test_features/test_opening_web_sites.py
@@ -25,3 +25,69 @@ def test_my_learning():
     open(url)
     test_speak('opening your programming learning')
     assert url.__contains__("my-learning")
+
+def test_facebook():
+    url = "https://www.facebook.com/"
+    open(url)
+    test_speak('opening facebook')
+    assert url.__contains__("facebook")
+
+def test_instagram():
+    url = "https://www.instagram.com/"
+    open(url)
+    test_speak('opening instagram')
+    assert url.__contains__("instagram")
+
+def test_whatsapp():
+    url = "https://web.whatsapp.com/"
+    open(url)
+    test_speak('opening whatsapp')
+    assert url.__contains__("whatsapp")
+
+def test_telegram():
+    url = "https://web.telegram.org/"
+    open(url)
+    test_speak('opening telegram')
+    assert url.__contains__("telegram")
+
+def test_github():
+    url = "https://github.com/"
+    open(url)
+    test_speak('opening github')
+    assert url.__contains__("github")
+
+def test_replit():
+    url = "https://replit.com/"
+    open(url)
+    test_speak('opening replit')
+    assert url.__contains__("replit")
+
+def test_gmail():
+    url = "https://mail.google.com/"
+    open(url)
+    test_speak('opening gmail')
+    assert url.__contains__("mail")
+
+def test_google():
+    url = "https://www.google.com"
+    open(url)
+    test_speak('opening google')
+    assert url.__contains__("google")
+
+def test_calendar():
+    url = "https://calendar.google.com/"
+    open(url)
+    test_speak('opening calendar')
+    assert url.__contains__("calendar")
+
+def test_pypi():
+    url = "https://pypi.org/"
+    open(url)
+    test_speak('opening python packages website')
+    assert url.__contains__("pypi")
+
+def test_chatgpt():
+    url = "https://chat.openai.com/"
+    open(url)
+    test_speak('opening chat g p t')
+    assert url.__contains__("chat")
