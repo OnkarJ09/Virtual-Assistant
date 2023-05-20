351726095dadf65f49eef4e96e8c208dd554f771
OJHacker <ojoshi387@gmail.com>
2023-05-20 14:06:06 +0530

    Added new web-sites in opening_web_sites Plugin

--- Virtual-Assistant/features/opening_web_sites.py
+++ Virtual-Assistant/features/opening_web_sites.py
@@ -16,3 +16,58 @@ def my_learning():
     url = "https://my-learning.w3schools.com/"
     open(url)
     speak('opening your programming learning')
+
+def facebook():
+    url = "https://www.facebook.com/"
+    open(url)
+    speak('opening facebook')
+
+def instagram():
+    url = "https://www.instagram.com/"
+    open(url)
+    speak('opening instagram')
+
+def whatsapp():
+    url = "https://web.whatsapp.com/"
+    open(url)
+    speak('opening whatsapp')
+
+def telegram():
+    url = "https://web.telegram.org/"
+    open(url)
+    speak('opening telegram')
+
+def github():
+    url = "https://github.com/"
+    open(url)
+    speak('opening github')
+
+def replit():
+    url = "https://replit.com/"
+    open(url)
+    speak('opening replit')
+
+def gmail():
+    url = "https://mail.google.com/"
+    open(url)
+    speak('opening gmail')
+
+def google():
+    url = "https://www.google.com"
+    open(url)
+    speak('opening google')
+
+def calendar():
+    url = "https://calendar.google.com/"
+    open(url)
+    speak('opening calendar')
+
+def pypi():
+    url = "https://pypi.org/"
+    open(url)
+    speak('opening python packages website')
+
+def chatgpt():
+    url = "https://chat.openai.com/"
+    open(url)
+    speak('opening chat g p t')
